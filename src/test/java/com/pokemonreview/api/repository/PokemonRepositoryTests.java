46305bfcf5e4fdf8a1e860e21f7c66490ece413f
kdseol <seolkd.e@gmail.com>
2023-07-24 13:29:00 +0900

    PokemonRepoTest getAll method 추가

--- src/test/java/com/pokemonreview/api/repository/PokemonRepositoryTests.java
+++ src/test/java/com/pokemonreview/api/repository/PokemonRepositoryTests.java
@@ -9,6 +9,8 @@ import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -32,6 +34,24 @@ class PokemonRepositoryTests {
         //Assert
         assertThat(savedPokemon).isNotNull();
         assertThat(savedPokemon.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void PokemonRepository_GetAll_ReturnMoreThenOnePokemon() {
+        Pokemon pokemon = Pokemon.builder()
+                .name("pikachu")
+                .type(PokemonType.ELECTRIC).build();
+        Pokemon pokemon2 = Pokemon.builder()
+                .name("Raichu")
+                .type(PokemonType.NORMAL).build();
+
+        pokemonRepository.save(pokemon);
+        pokemonRepository.save(pokemon2);
+
+        List<Pokemon> pokemonList = pokemonRepository.findAll();
+
+        Assertions.assertThat(pokemonList).isNotNull();
+        Assertions.assertThat(pokemonList.size()).isEqualTo(2);
     }
 
 }
