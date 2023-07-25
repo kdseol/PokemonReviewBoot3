3ce1d8951b98556377e4f1b173b790311424bb5a
kdseol <seolkd.e@gmail.com>
2023-07-25 10:46:34 +0900

    401, 403 handler add

---
+++ src/main/java/com/pokemonreview/api/config/AppConfig.java
@@ -0,0 +1,31 @@
+package com.pokemonreview.api.config;
+
+import com.pokemonreview.api.models.Pokemon;
+import com.pokemonreview.api.models.PokemonType;
+import com.pokemonreview.api.repository.PokemonRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Configuration
+public class AppConfig {
+    //10건의 더미데이터 만들기
+    @Bean
+    public CommandLineRunner test(PokemonRepository pokemonRepository) {
+        return args -> {
+            pokemonRepository.deleteAll();
+            List<Pokemon> pokemonList = IntStream.range(0, 10)
+                    .mapToObj(i -> Pokemon.builder()
+                            .name("pikachu" + i)
+                            .type(PokemonType.ELECTRIC)
+                            .build())
+                    .collect(Collectors.toList());
+
+            pokemonRepository.saveAll(pokemonList);
+        };
+    }
+}
