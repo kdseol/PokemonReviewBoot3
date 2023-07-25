4e6b9dc09062a2233244faf5fb1bb38cd0c9627c
kdseol <seolkd.e@gmail.com>
2023-07-25 10:19:44 +0900

    authcontroller login

--- src/main/java/com/pokemonreview/api/controllers/AuthController.java
+++ src/main/java/com/pokemonreview/api/controllers/AuthController.java
@@ -1,15 +1,22 @@
 package com.pokemonreview.api.controllers;
 
+import com.pokemonreview.api.dto.AuthResponseDto;
+import com.pokemonreview.api.dto.LoginDto;
 import com.pokemonreview.api.dto.UserDto;
 import com.pokemonreview.api.models.Role;
 import com.pokemonreview.api.models.UserEntity;
 import com.pokemonreview.api.repository.RoleRepository;
 import com.pokemonreview.api.repository.UserRepository;
 
+import com.pokemonreview.api.security.JWTGenerator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,18 +32,40 @@ public class AuthController {
     private final UserRepository userRepository; //생성자 주입 방식
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JWTGenerator jwtGenerator;
 
 //		public AuthController(UserRepository userRepository,
 //                          RoleRepository roleRepository,
-//                          PasswordEncoder passwordEncoder) {
+//                          PasswordEncoder passwordEncoder,
+//                          AuthenticationManager authenticationManager,
+//                          JWTGenerator jwtGenerator) {
 //        this.userRepository = userRepository;
 //        this.roleRepository = roleRepository;
-//        this.passwordEncoder = passwordEncoder;
+//        this.passwordEncoder = passwordEncoder
+//        this.authenticationManager = authenticationManager;
+//        this.jwtGenerator = jwtGenerator;;
 //    }
 
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome this endpoint is not secure";
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDto> login(@RequestBody LoginDto loginDto){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginDto.getUsername(),
+                        loginDto.getPassword())
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String token = jwtGenerator.generateToken(authentication);
+        AuthResponseDto authResponseDTO = new AuthResponseDto(token);
+        authResponseDTO.setUsername(loginDto.getUsername());
+        UserEntity userEntity = userRepository.findByUsername(loginDto.getUsername()).get();
+        authResponseDTO.setRole(userEntity.getRoles().get(0).getName());
+        return new ResponseEntity<>(authResponseDTO, HttpStatus.OK);
     }
 
     @PostMapping("/register")
